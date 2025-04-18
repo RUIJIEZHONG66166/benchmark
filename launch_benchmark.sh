af55f685eafc649fef39979eda1526b4a72d4b11
jundu <jundu>
2025-04-18 00:36:24 -0700

    update torchbench in OOB script

---
+++ launch_benchmark.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+set -xe
+
+function main {
+    source oob-common/common.sh
+    # set common info
+    init_params $@
+    fetch_device_info
+    set_environment
+
+    # requirements
+
+    cp oob-common/context_func.py ./
+    # if multiple use 'xxx,xxx,xxx'
+    model_name_list=($(echo "${model_name}" |sed 's/,/ /g'))
+    batch_size_list=($(echo "${batch_size}" |sed 's/,/ /g'))
+
+    # generate benchmark
+    for model_name in ${model_name_list[@]}
+    do
+        #
+        for batch_size in ${batch_size_list[@]}
+        do
+            # clean workspace
+            logs_path_clean
+
+            # generate launch script for multiple instance
+            if [ "${OOB_USE_LAUNCHER}" == "1" ] && [ "${device}" == "cpu" ];then
+                generate_core_launcher
+            else
+                generate_core
+            fi
+            # launch
+            echo -e "\n\n\n\n Running..."
+            source ${excute_cmd_file}
+            echo -e "Finished.\n\n\n\n"
+            # collect launch result
+            torchbench_latency=1 collect_perf_logs
+        done
+    done
+}
+
+# run
+function generate_core {
+    # generate multiple instance script
+    for(( i=0; i<instance; i++ ))
+    do
+        real_cores_per_instance=$(echo ${device_array[i]} |awk -F, '{print NF}')
+        log_file="${log_dir}/rcpi${real_cores_per_instance}-ins${i}.log"
+
+        # instances
+        if [ "${device}" == "cpu" ];then
+            OOB_EXEC_HEADER=" numactl -m $(echo ${device_array[i]} |awk -F ';' '{print $2}') "
+            OOB_EXEC_HEADER+=" -C $(echo ${device_array[i]} |awk -F ';' '{print $1}') "
+        elif [ "${device}" == "cuda" ];then
+            OOB_EXEC_HEADER=" CUDA_VISIBLE_DEVICES=${device_array[i]} "
+        elif [ "${device}" == "xpu" ];then
+            OOB_EXEC_HEADER=" ZE_AFFINITY_MASK=${i} "
+	      fi
+        if [ "${channels_last}" == "1" ];then
+            channels_last="--channels-last"
+        else
+            channels_last=""
+        fi
+        if [ "${precision}" == "float16" ];then
+            precision="fp16"
+        elif [ "${precision}" == "bfloat16" ];then
+            precision="bf16"
+        else
+            precision="fp32"
+        fi
+        printf " ${OOB_EXEC_HEADER} \
+	        python run.py \
+            ${model_name} \
+            -d ${device} --precision ${precision} --bs ${batch_size} \
+            ${channels_last} \
+            ${addtion_options} \
+            > ${log_file} 2>&1 &  \n" |tee -a ${excute_cmd_file}
+        if [ "${numa_nodes_use}" == "0" ];then
+            break
+        fi
+    done
+    echo -e "\n wait" >> ${excute_cmd_file}
+}
+
+function generate_core_launcher {
+    # generate multiple instance script
+    for(( i=0; i<instance; i++ ))
+    do
+        real_cores_per_instance=$(echo ${device_array[i]} |awk -F, '{print NF}')
+        log_file="${log_dir}/rcpi${real_cores_per_instance}-ins${i}.log"
+
+        printf "python -m launch --enable_jemalloc \
+                    --core_list $(echo ${device_array[@]} |sed 's/;.//g') \
+                    --log_file_prefix rcpi${real_cores_per_instance} \
+                    --log_path ${log_dir} \
+                    --ninstances ${#device_array[@]} \
+                    --ncore_per_instance ${real_cores_per_instance} \
+            tools/infer.py --weights $CKPT_DIR \
+                --source $DATASET_DIR \
+                --num_iter $num_iter --num_warmup $num_warmup \
+                --channels_last $channels_last --precision $precision \
+                ${addtion_options} \
+        > /dev/null 2>&1 &  \n" |tee -a ${excute_cmd_file}
+        break
+    done
+    echo -e "\n wait" >> ${excute_cmd_file}
+    # download launcher
+    wget --no-proxy -O launch.py http://mengfeil-ubuntu.sh.intel.com/share/launch.py
+}
+
+# download common files
+rm -rf oob-common && git clone https://github.com/intel-sandbox/oob-common.git -b gpu_oob
+
+# Start
+main "$@"
