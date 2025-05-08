1e97f26876011c8afb108f54e637fbb1bb01e2de
Du, Jun <jun.du@intel.com>
2025-05-08 03:22:34 +0000

    update launch_benchmark.sh

--- launch_benchmark.sh
+++ launch_benchmark.sh
@@ -13,7 +13,8 @@ function main {
     #python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
     pip install timm
     pip install --no-deps -r requirements.txt
-    #python install.py --continue_on_fail
+    torchbench_model_name=`echo ${model_name} | cut -d'_' -f2-`
+    python install.py ${torchbench_model_name} --continue_on_fail
 
     cp oob-common/context_func.py ./
     # if multiple use 'xxx,xxx,xxx'
