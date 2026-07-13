a56838b948a619f01e4cdfd4b71ae459d3d659af
Zhong, Ruijie <ruijie.zhong@intel.com>
2026-07-13 00:31:34 -0700

    fix the timm set issue

--- launch_benchmark.sh
+++ launch_benchmark.sh
@@ -14,8 +14,8 @@ function main {
     python -m pip install timm
     python -m pip install --no-deps -r requirements.txt
     python -m pip install -U pip wheel setuptools==81.0.0
-    python -m pip install --no-deps -U timm
     python install.py ${MODEL_NAME} --continue_on_fail
+    python -m pip install --no-deps -U timm
 
     cp oob-common/context_func.py ./
     # if multiple use 'xxx,xxx,xxx'
